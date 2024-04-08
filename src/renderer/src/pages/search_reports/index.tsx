b2a75eb71e24c160b6bdd72abfc77f8e86a83831
Isaac Garcia <124853347+isaacgarc-dev@users.noreply.github.com>
2024-04-07 21:34:34 -0300

    unused vars

--- src/renderer/src/pages/search_reports/index.tsx
+++ src/renderer/src/pages/search_reports/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Typography, Box, useTheme, Button } from "@mui/material";
 import {
   FlexBetween,
