e9e81565c00c9cc60f86f3a4c84b96bd0be95d0d
Isaac Garcia <124853347+isaacgarc-dev@users.noreply.github.com>
2024-04-09 12:25:23 -0300

    Merge conflict APIRequestSettings

--- src/main/services/APIRequestSettingService.ts
+++ src/main/services/APIRequestSettingService.ts
@@ -2,10 +2,7 @@ import { IRequestSetting } from "src/renderer/src/interface/IRequestSetting";
 import * as fs from "fs";
 import { promisify } from "util";
 import { DirectorySettingService } from "./DirectorySettingService";
-<<<<<<< HEAD
 import { writeFile } from "../utils/files";
-=======
->>>>>>> origin/main
 
 const readFileAsync = promisify(fs.readFile);
 /**
@@ -26,11 +23,7 @@ export class APIRequestSettingService {
   async saveSettings(settings: IRequestSetting): Promise<void> {
     const data = JSON.stringify(settings, null, 2);
     try {
-<<<<<<< HEAD
       await writeFile(this.reqSettingsDir, data);
-=======
-      await writeFileAsync(this.reqSettingsDir, data);
->>>>>>> origin/main
     } catch (err) {
       console.error("There was an error writing the file", err);
       throw err;
